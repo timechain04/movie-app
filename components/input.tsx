19e08e311ba5633382c189a897bb3e5246e1b09b
Andrii Lutsiak <timechain04@gmail.com>
2023-08-11 23:50:04 +0300

    form added size

--- components/input.tsx
+++ components/input.tsx
@@ -1,6 +1,27 @@
-const Input = () => {
+import React from "react";
+
+interface InputProps {
+    id: string;
+    onChange: any;
+    value: string;
+    label: string;
+    type?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+    id,
+    onChange,
+    value,
+    label,
+    type,
+}) => {
     return (
+        <div className="relative">
         <input
+        type={type}
+        onChange={onChange}
+        value={value}
+        id={id}
         className="
         block
         rounded-md
@@ -18,6 +39,28 @@ const Input = () => {
         "
         placeholder=" "
         />
+        <label
+        className="
+        absolute
+        text-md
+        text-zinc-400
+        duration-150
+        transform
+        -translate-y-3
+        scale-75
+        top-4
+        z-10
+        origin-[0]
+        left-6
+        peer-placeholder-shown:scale-100
+        peer-placeholder-shown:translate-y-0
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+        "
+        htmlFor={id}>
+            {label}
+        </label>
+        </div>
     )
 }
 
