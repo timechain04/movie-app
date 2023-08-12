08d364769482d329e86d6695e9a023ed77e65650
Andrii Lutsiak <timechain04@gmail.com>
2023-08-12 11:20:06 +0300

    toggle register

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -21,15 +21,17 @@ const Auth = () => {
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w2/5 lg:max-w-md rounded-md w-full">
                  <h2 className="text-white text-4xl mb-8 font-semibold">
-                     {variant === 'login' ? 'Sign in' : 'Create an account'}
+                     {variant === 'login' ? 'Sign in' : 'Register'}
                  </h2>
                  <div className="flex flex-col gap-4">
+                    {variant === 'register' && (
                  <Input
                    label="Username"
                    onChange={(ev: any) => setName(ev.target.value)}
                    id="name"
                    value={name}
                    />
+                   )}
                    <Input
                    label="Email"
                    onChange={(ev: any) => setEmail(ev.target.value)}
