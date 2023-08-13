822098157e70df21f5e6784a03963b3f456dc032
Andrii Lutsiak <timechain04@gmail.com>
2023-08-13 21:16:14 +0300

    nextauth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
+import axios from 'axios';
 import Input from "@/components/Input";
-
+import { signIn } from 'next-auth/react';
 
 const Auth = () => {
     const [email, setEmail] = useState('');
@@ -13,8 +14,29 @@ const Auth = () => {
     }, []);
 
     const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password]);
 
-    }, []);
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password]);
 
     return(
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -51,7 +73,7 @@ const Auth = () => {
                    value={password}
                    />
                  </div>
-                 <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                 <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                     {variant === 'login' ? 'Login' : 'Sign up'}
                  </button>
                  <p className="text-neutral-500 mt-12">
