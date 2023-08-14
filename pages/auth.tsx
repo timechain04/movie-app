7ceb8a8076b94f2bcdad24cc928d7767799df897
Andrii Lutsiak <timechain04@gmail.com>
2023-08-14 09:29:12 +0300

    setup login

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,8 +2,11 @@ import { useCallback, useState } from "react";
 import axios from 'axios';
 import Input from "@/components/Input";
 import { signIn } from 'next-auth/react';
+import { useRouter } from "next/router";
 
 const Auth = () => {
+    const router = useRouter();
+
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -13,18 +16,6 @@ const Auth = () => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, []);
 
-    const register = useCallback(async () => {
-        try {
-            await axios.post('/api/register', {
-                email,
-                name,
-                password,
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }, [email, name, password]);
-
     const login = useCallback(async () => {
         try {
             await signIn('credentials', {
@@ -33,10 +24,26 @@ const Auth = () => {
                 redirect: false,
                 callbackUrl: '/'
             });
+            router.push('/');
         } catch (error) {
             console.log(error);
         }
-    }, [email, password]);
+    }, [email, password, router]);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password,
+            });
+            login();
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password, login]);
+
+
 
     return(
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -73,7 +80,7 @@ const Auth = () => {
                    value={password}
                    />
                  </div>
-                 <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                 <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                     {variant === 'login' ? 'Login' : 'Sign up'}
                  </button>
                  <p className="text-neutral-500 mt-12">
